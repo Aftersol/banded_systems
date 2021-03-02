0ba2a433df1870fa87384cce4aa9ff33be8b8c1a
Aftersol <42478428+Aftersol@users.noreply.github.com>
2021-03-02 14:43:07 -0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Eater of Worlds/Desktop/banded_systems
-# Build directory: C:/Users/Eater of Worlds/Desktop/banded_systems/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
